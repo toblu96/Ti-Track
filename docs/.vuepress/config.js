989c3931577ce6242cc2d4842be6780a950228a5
Tobias Blum <tobias.blum@besonet.ch>
2020-04-20 17:34:06 +0200

    display all sidebar navigations

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -37,6 +37,7 @@ module.exports = {
         },
         searchPlaceholder: 'Search...',
 
+        displayAllHeaders: true,
         sidebar: {
             '/about/': [
               'aboutProject',
